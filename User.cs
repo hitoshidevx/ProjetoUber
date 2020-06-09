da7e99304364ea288356290b8790672f347264ab
unknown <g.hitoshi.yokogawa@gmail.com>
2020-06-09 10:00:31 -0300

    User finalizado

--- User.cs
+++ User.cs
@@ -3,13 +3,34 @@ namespace ProjetoUber
     public class User
     {
         public string Nome { get; set; }
-        private string Login = "gabriel@gmail.com";
-        private string Senha = "12345678";
+        private string login = "gabriel@gmail.com";
+        private string senha = "12345678";
         public string Foto { get; set; }
         public string LocalizacaoAtual { get; set; }
         public string TokenLogin { get; set; }
 
-        
+        /// <summary>
+        /// Login.false
+        /// </summary>
+        /// <param name="login"></param>
+        /// <returns>O retorno depende do insert do usuário, caso esteja nas conformidades citadas acima, ele obtém acesso.</returns>
+        public bool Login(string login, string senha){
+
+            if(this.login == login && this.senha == senha){
+                
+                TokenLogin = "iquwqieiu823827uqwyiuq";
+                return true;
+            }
+
+            return false;
+        }
+        /// <summary>
+        /// Logout.
+        /// Retorna o logout.
+        /// </summary>
+        public void Logout(){
+            TokenLogin = "";
+        }
         
     }
 }
