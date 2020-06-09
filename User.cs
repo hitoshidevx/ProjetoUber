8229397cbacc42b96a1704b3a50d9100edf3b18d
unknown <g.hitoshi.yokogawa@gmail.com>
2020-06-09 09:52:10 -0300

    Projeto Uber inicializado

---
+++ User.cs
@@ -0,0 +1,15 @@
+namespace ProjetoUber
+{
+    public class User
+    {
+        public string Nome { get; set; }
+        private string Login = "gabriel@gmail.com";
+        private string Senha = "12345678";
+        public string Foto { get; set; }
+        public string LocalizacaoAtual { get; set; }
+        public string TokenLogin { get; set; }
+
+        
+        
+    }
+}
